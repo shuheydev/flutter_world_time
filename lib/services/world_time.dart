f7aa1d67a0c54b8f49d2a2be8ba09b02a0f59287
shuheydev <shuhey.dev@gmail.com>
2020-07-27 23:49:23 +0900

    WebAPIからデータ取得時のエラーハンドリングを追加した.

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,24 +11,29 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    //make the request
-    http.Response response =
-        await http.get('http://worldtimeapi.org/api/timezone/Asia/Tokyo');
-    Map data = jsonDecode(response.body);
-    // print(data);
+    try {
+      //make the request
+      http.Response response =
+          await http.get('http://worldtimeapi.org/api/timezones/Asia/Tokyo');
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    //get properties from data
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(dateTime);
-    // print(offset);
+      //get properties from data
+      String dateTime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      // print(dateTime);
+      // print(offset);
 
-    //create DateTime object
-    DateTime now = DateTime.parse(dateTime);
-    int offsetInt = int.parse(offset);
-    now = now.add(Duration(hours: offsetInt));
+      //create DateTime object
+      DateTime now = DateTime.parse(dateTime);
+      int offsetInt = int.parse(offset);
+      now = now.add(Duration(hours: offsetInt));
 
-    //set time property
-    this.time = now.toString();
+      //set time property
+      this.time = now.toString();
+    } catch (e) {
+      print('caught error: $e');
+      this.time = 'could not get time data';
+    }
   }
 }
