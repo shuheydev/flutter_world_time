93a0ce99cef147cbdacec7587bc9696b0aae855c
shuheydev <shuhey.dev@gmail.com>
2020-07-28 10:32:32 +0900

    昼時間帯と夜時間帯で背景画像と色を変更するようにした.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,53 +12,74 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
-
     print('arguments: $data');
+
     WorldTime wt = data['instance'] as WorldTime;
     print('instance: ${wt.time}');
     print('location: ${data['location']}');
 
+    //set background
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(
-                    context,
-                    '/location',
-                  );
-                },
-                icon: Icon(
-                  Icons.edit_location,
-                ),
-                label: Text(
-                  'Edit location',
-                ),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/$bgImage',
               ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      '/location',
+                    );
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit location',
                     style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
+                      color: Colors.grey[300],
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
                 ),
-              )
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
